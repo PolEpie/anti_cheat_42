f49838791913d3fed5865f182f62ad981d95bf25
Pol <polepie@gmail.com>
2023-09-10 00:35:13 +0200

    First version

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# anti_cheat
+# 🚔 Anti Cheat 42
+
+Hi! If like me your sometimes do you debuging your code using printf and you dont wanna be marked as cheater by the Moulinette this program is for you !
+
+It will directly read all the .c files on the current directory and into all the subdirectories too and let you know if any of there a **printf** or even if you still have a **main** to avoid send your testing main
+
+Also it will find every **a.out** files to delete them and also let you know if there is others files that `.c` or `.h` ones
+
+Btw it avoid commented lines things so if you using `/* */` or `//` comments, anti cheat will not send you any message 😉
+
+If you wanna another feature into it let me know on issues tab 😁
+
+# Compilation
+
+    gcc anti_cheat.c -o anti_cheat
+
+## Usage
+
+    ./anti_cheat
+
+![enter image description here](https://cdn.discordapp.com/attachments/664897058867511328/1150196989343039600/CleanShot_2023-09-10_at_00.31.492x.png)
---
+++ anti_cheat.c
@@ -0,0 +1,160 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   anti_cheat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pepie <pepie@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/09 23:26:21 by pepie             #+#    #+#             */
+/*   Updated: 2023/09/09 23:26:29 by pepie            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <limits.h>
+#include <unistd.h>
+
+
+int verify_printf(FILE *file)
+{
+    int is_commented;
+    int commented_line;
+    int c;
+    char last_char;
+    int is_in_print;
+    int is_in_main;
+    int line_number;
+
+    is_commented = 0;
+    is_in_print = 0;
+    is_in_main = 0;
+    commented_line = 0;
+    line_number = 1;
+    while ((c = fgetc(file)) != EOF)
+    {
+        if (last_char == "/" && c == '*')
+            is_commented = 1;
+
+        if (last_char == "*" && c == '/')
+            is_commented = 0;
+
+        if (last_char == '/' && c == '/')
+            commented_line = 1;
+
+        if (c == '\n')
+        {
+            commented_line = 0;
+            line_number++;
+        }    
+
+        if (c == 'p' && commented_line == 0 && is_commented == 0 && (last_char == '\t' || last_char == ' '))
+            is_in_print = 1;
+        else if (c == 'r' && last_char == 'p' && is_in_print )
+            is_in_print = 1;
+        else if (c == 'i' && last_char == 'r' && is_in_print)
+            is_in_print = 1;
+        else if (c == 'n' && last_char == 'i' && is_in_print)
+            is_in_print = 1;
+        else if (c == 't' && last_char == 'n' && is_in_print)
+            is_in_print = 1;
+        else if (c == 'f' && last_char == 't' && is_in_print)
+            is_in_print = 1;
+        else if (c == '(' && last_char == 'f' && is_in_print)
+            printf("[ ERROR ] printf detected at line: %d\n", line_number);
+        else
+            is_in_print = 0;
+
+        if (c == 'm' && commented_line == 0 && is_commented == 0 && (last_char == '\t' || last_char == ' '))
+            is_in_main = 1;
+        else if (c == 'a' && last_char == 'm' && is_in_main)
+            is_in_main = 1;
+        else if (c == 'i' && last_char == 'a' && is_in_main)
+            is_in_main = 1;
+        else if (c == 'n' && last_char == 'i' && is_in_main)
+            is_in_main = 1;
+        else if (c == '(' && last_char == 'n' && is_in_main)
+            printf("[ MAIN ] main detected at line: %d\n", line_number);
+        else
+            is_in_main = 0;
+
+        last_char = c;
+    }
+}
+
+void findCFiles(const char *dir) {
+    DIR *dp;
+    struct dirent *entry;
+
+    dp = opendir(dir);
+
+    if (dp == NULL) {
+        perror("opendir");
+        return;
+    }
+
+    while ((entry = readdir(dp))) {
+        if (entry->d_type == DT_DIR) {
+            // Ignore "." and ".." directories
+            if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0 || strcmp(entry->d_name, ".git") == 0) {
+                continue;
+            }
+            
+            // Recursively call the function for subdirectories
+            char subDirPath[256];
+            snprintf(subDirPath, sizeof(subDirPath), "%s/%s", dir, entry->d_name);
+            findCFiles(subDirPath);
+        } else if (entry->d_type == DT_REG) {
+            // Check if the file has a .c extension
+            char *extension = strrchr(entry->d_name, '.');
+            if (extension && strcmp(extension, ".c") == 0) {
+                printf("________________\n");
+                printf("Found C file: %s/%s\n", dir, entry->d_name);
+                FILE *file = fopen(entry->d_name, "r");
+
+                verify_printf(file);
+
+                if (file == NULL) {
+                    perror("Error opening file");
+                }
+                fclose(file);
+            }
+            else if (strcmp(entry->d_name, "a.out") == 0)
+            {
+                printf("______________\n");
+                printf("| build_file | Found a.out file: %s/%s\n", dir, entry->d_name);
+
+                if (remove(entry->d_name) == 0) {
+                    printf("File %s removed successfully.\n", entry->d_name);
+                } else {
+                    perror("Error removing file");
+                }
+            }
+            else
+            {
+                printf("______________\n");
+                printf("| other file | Found file: %s/%s (i'll not delete it)\n", dir, entry->d_name);
+            }
+        }
+    }
+
+    closedir(dp);
+}
+
+int main(int argc, char **argv)
+{
+    char cwd[PATH_MAX];
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL) {
+       perror("getcwd() error");
+       return 1;
+    }
+    
+    const char *dir = cwd;
+    printf("Current working dir: %s\n", dir);
+    findCFiles(dir);
+
+    return (0);
+}
+
